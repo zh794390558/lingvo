de0bb7c21ecc10e6333e16f70d21cd20b767fa9b
Hui Zhang <zhtclz@foxmail.com>
2020-02-10 04:29:03 +0000

    change brown corpus run.sh

--- egs/brown_corpus/run.sh
+++ egs/brown_corpus/run.sh
@@ -1,7 +1,9 @@
 
 stage=$1
 model=punctuator.codelab.RNMTModel
-logdir=$PWD/egs/brown_corpus/exp/$model
+#logdir=$PWD/egs/brown_corpus/exp/$model
+logdir=/nfs/project/zhanghui/lingvo/brown_corpus/logdir
+mkdir -p logdir
 
 echo stage: $stage
 echo workdir: $PWD
@@ -13,29 +15,27 @@ if [ $stage -le 0 ] && [ $stage -ge 0 ];then
   bazel run -c opt --distdir=~/dist lingvo/tasks/punctuator/tools:download_brown_corpus --  --outdir="/nfs/project/datasets/opensource_data/brown_corpus"
 fi
 
-
-if [ $stage -le 1 ] && [ $stage -ge 1 ];then 
-   # train and eval 
-   echo "Training..." 
+if [ $stage -le 1 ] && [ $stage -ge 1 ];then
+   # train and eval
+   echo "Training..."
    bazel run -c opt --distdir=~/dist --config=cuda //lingvo:trainer --  \
     --mode=sync    \
     --job=controller,trainer_client \
     --run_locally=gpu    \
     --worker_gpus=1   \
-    --worker_split_size=1     \
+    --worker_split_size=1  \
     --model=$model \
     --logdir=$logdir \
-    --saver_max_to_keep=10     \
-    --saver_keep_checkpoint_every_n_hours=100000.0    \
+    --saver_max_to_keep=10  \
+    --saver_keep_checkpoint_every_n_hours=0.0 \
     --alsologtostderr
 fi
 
 if [ $stage -le 2 ] && [ $stage -ge 2 ];then
-  # eval dev
-  echo "Eval dev..."
+  echo "Eval test..."
   bazel run -c opt --distdir=~/dist --config=cuda //lingvo:trainer --  \
     --mode=sync    \
-    --job=evaler_dev,decoder_dev \
+    --job=evaler_test\
     --run_locally=cpu     \
     --model=$model \
     --logdir=$logdir \
@@ -43,8 +43,7 @@ if [ $stage -le 2 ] && [ $stage -ge 2 ];then
 fi
 
 if [ $stage -le 3 ] && [ $stage -ge 3 ];then
-  # eval test
-  echo "Eval test..."
+  echo "Decode test..."
   bazel run -c opt --distdir=~/dist --config=cuda //lingvo:trainer --  \
     --mode=sync    \
     --job=decoder_test \
